09f86f7ce09962cbaa8dd9c2525e347c97004a6f
Ali <biz.alihabibi@gmail.com>
2024-08-17 10:52:59 +0330

    Setup viewBinding

--- app/src/main/java/com/ir/ali/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/ir/ali/bmi_calculator/MainActivity.kt
@@ -2,10 +2,13 @@ package com.ir.ali.bmi_calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ir.ali.bmi_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
