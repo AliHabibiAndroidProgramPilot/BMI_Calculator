ad467575c18bae8358e84ee347e2452dc918a68d
Ali <biz.alihabibi@gmail.com>
2024-08-18 11:50:46 +0330

    Buttons functionality(increase & decrease weight and age)

--- app/src/main/java/com/ir/ali/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/ir/ali/bmi_calculator/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.ir.ali.bmi_calculator
 
+import android.annotation.SuppressLint
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.os.Handler
 import android.widget.SeekBar
-import android.widget.Toast
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.ir.ali.bmi_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -43,5 +46,65 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+        binding.weightDecrease.setOnClickListener {
+            binding.weightNumber.text =
+                (binding.weightNumber.text.toString().toInt() - 1).toString()
+        }
+        binding.weightDecrease.setOnLongClickListener {
+            consistDecrease(binding.weightNumber, binding.weightDecrease)
+            true
+        }
+        binding.weightAdd.setOnClickListener {
+            binding.weightNumber.text =
+                (binding.weightNumber.text.toString().toInt() + 1).toString()
+        }
+        binding.weightAdd.setOnLongClickListener {
+            consistIncrease(binding.weightNumber, binding.weightAdd)
+            true
+        }
+        binding.ageDecrease.setOnClickListener {
+            binding.ageNumber.text = binding.ageNumber.text.toString().toInt().minus(1).toString()
+        }
+        binding.ageDecrease.setOnLongClickListener {
+            consistDecrease(binding.ageNumber, binding.ageDecrease)
+            true
+        }
+        binding.ageAdd.setOnClickListener {
+            binding.ageNumber.text = (binding.ageNumber.text.toString().toInt() + 1).toString()
+        }
+        binding.ageAdd.setOnLongClickListener {
+            consistIncrease(binding.ageNumber, binding.ageAdd)
+            true
+        }
+
+    }
+
+    private val handler = Handler()
+    private val delayMillis: Long = 100
+
+    private fun consistIncrease(textView: TextView, button: FloatingActionButton) {
+        var runnable: Runnable? = null
+        runnable = Runnable {
+            if(button.isPressed) {
+                val tempNumber = textView.text.toString().toInt()
+                val tempText = tempNumber.plus(1).toString()
+                handler.postDelayed(runnable!!, delayMillis)
+                textView.text = tempText
+            }
+        }
+        handler.post(runnable)
+    }
+
+    private fun consistDecrease(textView: TextView, button: FloatingActionButton) {
+        var runnable: Runnable? = null
+        runnable = Runnable {
+            if(button.isPressed) {
+                val tempNumber = textView.text.toString().toInt()
+                val tempText = tempNumber.minus(1).toString()
+                handler.postDelayed(runnable!!, delayMillis)
+                textView.text = tempText
+            }
+        }
+        handler.post(runnable)
     }
 }
